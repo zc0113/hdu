c63dc757a286c1358dd7d8335dba707a55f71d5d
zc0113 <46887620+zc0113@users.noreply.github.com>
2020-07-03 09:59:15 +0800

    add fun

--- hdu2585.cpp
+++ hdu2585.cpp
@@ -7,6 +7,25 @@ using namespace std;
 
 int dp[MAX_SIZE][MAX_SIZE];
 
+int scmp(char* s, char* s1) {
+	int len1 = strlen(s + 1);
+	int len2 = strlen(s1 + 1);
+	for (int i = 1; i <= len1; ++i) {
+		for (int j = 1; j <= len2; ++j) {
+			if (s[i] == '?' && dp[i - 1][j - 1]) {
+				dp[i][j] = 1;
+			} else if (s[i] == '*') {
+				if (dp[i - 1][j - 1]) dp[i][j] = 1;
+				if (dp[i - 1][j]) dp[i][j] = 1;
+				if (dp[i][j - 1]) dp[i][j] = 1;
+			} else if (s[i] == s1[j] && dp[i-1][j-1]) {
+				dp[i][j] = 1;
+			}
+		}
+	}
+	return dp[len1][len2];
+} 
+
 int main() {
 	char s[MAX_SIZE];
 	int n;
@@ -18,22 +37,7 @@ int main() {
 			cin >> (s1 + 1);
 			memset(dp, 0, sizeof(dp));
 			dp[0][0] = 1;
-			int len1 = strlen(s + 1);
-			int len2 = strlen(s1 + 1);
-			for (int i = 1; i <= len1; ++i) {
-				for (int j = 1; j <= len2; ++j) {
-					if (s[i] == '?' && dp[i - 1][j - 1]) {
-						dp[i][j] = 1;
-					} else if (s[i] == '*') {
-						if (dp[i - 1][j - 1]) dp[i][j] = 1;
-						if (dp[i - 1][j]) dp[i][j] = 1;
-						if (dp[i][j - 1]) dp[i][j] = 1;
-					} else if (s[i] == s1[j] && dp[i-1][j-1]) {
-						dp[i][j] = 1;
-					}
-				}
-			}
-			if (dp[len1][len2]) count++;
+			if (scmp(s, s1)) count++;
 		}
 		cout << count << endl;
 	}
